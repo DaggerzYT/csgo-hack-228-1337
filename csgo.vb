626982cf97a6ba6a6e0a7452ab84055a948e63de
DaggerzYT <64489587+DaggerzYT@users.noreply.github.com>
2021-12-28 17:55:24 +0300

    Add files via upload

---
+++ csgo.vb
@@ -0,0 +1,16 @@
+' 2021-12-28 14:49:01.615523100 UTC
+
+Namespace hazedumper
+    Public Shared Class netvars
+        Public Const cs_gamerules_data as Integer = &H0
+        Public Const m_ArmorValue as Integer = &H117CC
+        Public Const m_Collision as Integer = &H320
+        Public Const m_CollisionGroup as Integer = &H474
+        Public Const m_Local as Integer = &H2FCC
+        Public Const m_MoveType as Integer = &H25C
+        Public Const m_OriginalOwnerXuidHigh as Integer = &H31D4
+        Public Const m_OriginalOwnerXuidLow as Integer = &H31D0
+        Public Const m_SurvivalGameRuleDecisionTypes as Integer = &H1328
+        Public Const m_SurvivalRules as Integer = &HD00
+        Public Const m_aimPunchAngle as Integer = &H303C
+        Public Const 
